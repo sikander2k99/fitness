c4dbffa694612812a2eaec8d616c3ad38c83a35a
sikander2k99 <140113260+sikander2k99@users.noreply.github.com>
2024-09-17 16:00:03 +0530

    Add files via upload
    aware about fitness

---
+++ fitforlife/login.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fitness_website";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['username'] = $username;
+            header("Location: welcome.php");
+        } else {
+            echo "Invalid password";
+        }
+    } else {
+        echo "No user found";
+    }
+}
+
+$conn->close();
+?>
